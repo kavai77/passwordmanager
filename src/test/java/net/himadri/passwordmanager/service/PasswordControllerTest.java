a7c848217740bd9cb444ca6acb34bf3667ad1cff
kavai77 <kavai.csaba@zoho.com>
2016-11-05 08:57:11 +0100

    Adding more tests

--- src/test/java/net/himadri/passwordmanager/service/PasswordControllerTest.java
+++ src/test/java/net/himadri/passwordmanager/service/PasswordControllerTest.java
@@ -5,9 +5,7 @@ import com.google.appengine.api.users.UserService;
 import com.googlecode.objectify.LoadResult;
 import com.googlecode.objectify.Objectify;
 import com.googlecode.objectify.Result;
-import com.googlecode.objectify.cmd.LoadType;
-import com.googlecode.objectify.cmd.Loader;
-import com.googlecode.objectify.cmd.Saver;
+import com.googlecode.objectify.cmd.*;
 import net.himadri.passwordmanager.entity.Password;
 import org.junit.Before;
 import org.junit.Test;
@@ -22,9 +20,12 @@ import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import java.util.Arrays;
+
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -54,10 +55,11 @@ public class PasswordControllerTest {
         when(ofy.save().entity(any(Password.class))).thenReturn(mock(Result.class));
         when(ofy.load()).thenReturn(mock(Loader.class));
         when(ofy.load().type(Password.class)).thenReturn(mock(LoadType.class));
+        when(ofy.delete()).thenReturn(mock(Deleter.class));
     }
 
     @Test
-    public void when_StoringPassword_Then_PasswordIsSaved_And_Returned() throws Exception {
+    public void given_GoodParameters_when_StoringPassword_Then_PasswordIsSaved_And_Returned() throws Exception {
         // when
         ResultActions resultActions = mockMvc.perform(
                 post("/secure/password/store")
@@ -77,7 +79,7 @@ public class PasswordControllerTest {
     }
 
     @Test
-    public void when_ChangingDomain_Then_NewPasswordIsSaved_And_Returned() throws Exception {
+    public void given_GoodParameters_when_ChangingDomain_Then_NewPasswordIsSaved_And_Returned() throws Exception {
         // given
         when(ofy.load().type(Password.class).id(1L)).thenReturn(mock(LoadResult.class));
         when(ofy.load().type(Password.class).id(1L).now()).thenReturn(
@@ -121,7 +123,7 @@ public class PasswordControllerTest {
     }
 
     @Test
-    public void when_ChangingUserName_Then_NewUserNameIsSaved_And_Returned() throws Exception {
+    public void given_GoodParameters_when_ChangingUserName_Then_NewUserNameIsSaved_And_Returned() throws Exception {
         // given
         when(ofy.load().type(Password.class).id(1L)).thenReturn(mock(LoadResult.class));
         when(ofy.load().type(Password.class).id(1L).now()).thenReturn(
@@ -164,5 +166,115 @@ public class PasswordControllerTest {
         verifyNoMoreInteractions(ofy.save());
     }
 
+    @Test
+    public void given_GoodParameters_when_ChangingHexAndIv_Then_NewValuesSaved_And_Returned() throws Exception {
+        // given
+        when(ofy.load().type(Password.class).id(1L)).thenReturn(mock(LoadResult.class));
+        when(ofy.load().type(Password.class).id(1L).now()).thenReturn(
+                new Password("userId", "domain", "userName", "hex", "iv"));
+
+        // when
+        ResultActions resultActions = mockMvc.perform(
+                post("/secure/password/changeHex")
+                        .param("id", "1")
+                        .param("hex", "newHex")
+                        .param("iv", "newIv")
+                        .accept(MediaType.APPLICATION_JSON_UTF8));
+
+        // then
+        Password expectedPassword = new Password("userId", "domain", "userName", "newHex", "newIv");
+        resultActions
+                .andExpect(status().isOk())
+                .andExpect(model().attribute("password", expectedPassword));
+
+        verify(ofy.save()).entity(expectedPassword);
+    }
+
+    @Test
+    public void given_wrongUserId_when_ChangingHexAndIv_Then_Fails() throws Exception {
+        // given
+        when(ofy.load().type(Password.class).id(1L)).thenReturn(mock(LoadResult.class));
+        when(ofy.load().type(Password.class).id(1L).now()).thenReturn(
+                new Password("otherUserId", null, null, null, null));
+
+        // when
+        ResultActions resultActions = mockMvc.perform(
+                post("/secure/password/changeHex")
+                        .param("id", "1")
+                        .param("hex", "newHex")
+                        .param("iv", "newIv")
+                        .accept(MediaType.APPLICATION_JSON_UTF8));
+
+        // then
+        resultActions
+                .andExpect(status().is4xxClientError());
+
+        verifyNoMoreInteractions(ofy.save());
+    }
+
+    @Test
+    public void given_GoodParameters_when_DeletingPassword_Then_HttpOk() throws Exception {
+        // given
+        when(ofy.load().type(Password.class).id(1L)).thenReturn(mock(LoadResult.class));
+        Password loadedPassword = new Password("userId", "domain", "userName", "hex", "iv");
+        when(ofy.load().type(Password.class).id(1L).now()).thenReturn(loadedPassword);
+        when(ofy.delete().entity(any(Password.class))).thenReturn(mock(Result.class));
+
+        // when
+        ResultActions resultActions = mockMvc.perform(
+                post("/secure/password/deletePassword")
+                        .param("id", "1")
+                        .accept(MediaType.APPLICATION_JSON_UTF8));
+
+        // then
+        resultActions
+                .andExpect(status().isOk());
+
+        verify(ofy.delete()).entity(loadedPassword);
+    }
+
+    @Test
+    public void given_wrongUserId_when_DeletingPassword_Then_Fails() throws Exception {
+        // given
+        when(ofy.load().type(Password.class).id(1L)).thenReturn(mock(LoadResult.class));
+        when(ofy.load().type(Password.class).id(1L).now()).thenReturn(
+                new Password("otherUserId", null, null, null, null));
+
+        // when
+        ResultActions resultActions = mockMvc.perform(
+                post("/secure/password/deletePassword")
+                        .param("id", "1")
+                        .accept(MediaType.APPLICATION_JSON_UTF8));
+
+        // then
+        resultActions
+                .andExpect(status().is4xxClientError());
+
+        verifyNoMoreInteractions(ofy.delete());
+    }
+
+    @Test
+    public void when_RetrievingPasswords_Then_PasswordSortedAndReturned() throws Exception {
+        // given
+        when(ofy.load().type(Password.class).filter(anyString(), anyString())).thenReturn(mock(Query.class));
+        when(ofy.load().type(Password.class).filter(anyString(), anyString()).order(anyString())).thenReturn(mock(Query.class));
+        Password pwd1 = new Password("userId", "BDomain", "userName", "hex", "iv");
+        Password pwd2 = new Password("userId", "aDomain", "userName", "hex", "iv");
+        when(ofy.load().type(Password.class).filter(anyString(), anyString()).order(anyString()).list())
+                .thenReturn(Arrays.asList(pwd1, pwd2));
+
+        // when
+        ResultActions resultActions = mockMvc.perform(
+                post("/secure/password/retrieve")
+                        .accept(MediaType.APPLICATION_JSON_UTF8));
+
+        // then
+        resultActions
+                .andExpect(status().isOk()).andDo(print())
+                .andExpect(model().attribute("passwordList", Arrays.asList(pwd2, pwd1)));
+
+        verify(ofy.load().type(Password.class)).filter("userId", "userId");
+        verify(ofy.load().type(Password.class).filter("userId", "userId")).order("domain");
+    }
 
 }
